39517a3bb4ecd86443e78f5e758d9fe467645c59
ilrdn <49016594+ilrdn@users.noreply.github.com>
2020-04-24 17:39:46 +0400

    start

---
+++ src/router/index.js
@@ -0,0 +1,10 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+/* import Uimini from '.../node_modules/uimini/src/stylus/main.styl'  */
+
+Vue.use(Router)
+/* Vue.use(Uimini) */
+
+export default new Router({
+  routes: []
+})
